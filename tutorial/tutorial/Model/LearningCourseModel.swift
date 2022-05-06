b6252ca34a386888c93c418695185175332ef102
Aphananthe <aphananthe42@gmail.com>
2022-05-06 15:55:49 +0900

    Fix: cell thumbnail

--- tutorial/tutorial/Model/LearningCourseModel.swift
+++ tutorial/tutorial/Model/LearningCourseModel.swift
@@ -18,32 +18,32 @@ struct LearningCourseModel {
     static func createModel() -> [LearningCourseModel] {
         return [
             LearningCourseModel(
-                thumbnail: UIImage(systemName: "square.fill"),
+                thumbnail: UIImage(named: "python"),
                 title: "python + Django3 Djangoを基礎から応用まで、アプリケーション開発マスターpython付き",
                 author: "NAOKI MATSUMOTO",
                 progress: 0.01),
             LearningCourseModel(
-                thumbnail: UIImage(systemName: "square.fill"),
+                thumbnail: UIImage(named: "docker"),
                 title: "ゼロからはじめる Dockerによるアプリケーション実行環境構築",
                 author: "Kazuya Kojima",
                 progress: 0.71),
             LearningCourseModel(
-                thumbnail: UIImage(systemName: "square.fill"),
+                thumbnail: UIImage(named: "vally"),
                 title: "現役シリコンバレーエンジニアが教えるPython3 入門 + + 応用 + アメリカのシリコンバレー流コードスタイル",
                 author: "酒井 潤(Jun Sakai)",
                 progress: 0.43),
             LearningCourseModel(
-                thumbnail: UIImage(systemName: "square.fill"),
+                thumbnail: UIImage(named: "aws"),
                 title: "【2022年版】これだけでOK! AWS認定クラウドプラクティショナー試験突破講座(豊富な試験問題290問付き)",
                 author: "Shingo Shibata/AWS certified solutions architect, AWS certified cloud practitioner, AZ-900, Edutech Global/AWS certified solutions architect, AWS certified cloud practitioner, AZ-900",
                 progress: 0.79),
             LearningCourseModel(
-                thumbnail: UIImage(systemName: "square.fill"),
+                thumbnail: UIImage(named: "spreadsheet"),
                 title: "無料で使える表計算Googleスプレッドシート3ステップ速習講座",
                 author: "小林靖彦 Yasuhiko Kobayashi, 仁美 浅見",
                 progress: 0.55),
             LearningCourseModel(
-                thumbnail: UIImage(systemName: "square.fill"),
+                thumbnail: UIImage(named: "ios"),
                 title: "【iOS13対応】未経験者がiPhoneアプリ開発者になるための全て iOS Boot Camp",
                 author: "BoxCom,inc Yuta Fujii",
                 progress: 0.33)
